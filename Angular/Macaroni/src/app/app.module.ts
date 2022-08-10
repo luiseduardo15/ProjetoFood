37a38b64775338fae66608da29f057626902e8e3
Luís Eduardo <dudusiilva_15@outlook.com>
2022-08-09 23:05:07 -0300

    v1.8

--- Angular/Macaroni/src/app/app.module.ts
+++ Angular/Macaroni/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { MenupageComponent } from './pages/menupage/menupage.component';
-import { LoginComponent } from './pages/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -22,8 +22,8 @@ import { LoginComponent } from './pages/login/login.component';
     AboutComponent,
     ContactComponent,
     MenupageComponent,
-    LoginComponent,
-   
+
+
   ],
   imports: [
     BrowserModule,
